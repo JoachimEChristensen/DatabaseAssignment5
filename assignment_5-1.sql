5fe68c09de917df308f256df7de8ea8d8b31f7d7
Xinkai Huang <stanley_h007@hotmail.com>
2019-03-02 05:10:17 +0100

    Update assignment_5-1.sql

--- assignment_5-1.sql
+++ assignment_5-1.sql
@@ -1,12 +1,8 @@
-use stackoverflow;
-
-CALL `stackoverflow`.`denormalizeComments`(1);
-
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `denormalizeComments`(IN postId int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `denormalizeComments`(IN postsID INT)
 BEGIN
-SELECT postId, JSON_OBJECT('Id', Id, 'PostId', PostId, 'Score', Score, 'Text', Text, 'CreationDate', CreationDate, 'UserId', UserId)
-AS Com 
-FROM comments WHERE comments.PostId = postId;
-END$$
-DELIMITER ;
+select posts.Id, JSON_ARRAYAGG(commentArray) as commentArrays
+from posts
+inner join(select PostId, JSON_OBJECT('CommentId', Id, 'PostId', PostId, 'Score', Score, 'Text', Text, 'CreationDate', CreationDate, 'UserId', UserId)
+as commentArray from comments) as comment_table on posts.Id = comment_table.PostId
+where postsID = comment_table.PostId;
+END
