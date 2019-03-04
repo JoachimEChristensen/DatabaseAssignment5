fd0292456c14026770110bc8e5de8e62879a15e7
Xinkai Huang <stanley_h007@hotmail.com>
2019-03-04 06:41:08 +0100

    Delete assignment_5-1.sql

--- assignment_5-1.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `denormalizeComments`(IN postsID INT)
-BEGIN
-select posts.Id, JSON_ARRAYAGG(commentArray) as commentArrays
-from posts
-inner join(select PostId, JSON_OBJECT('CommentId', Id, 'PostId', PostId, 'Score', Score, 'Text', Text, 'CreationDate', CreationDate, 'UserId', UserId)
-as commentArray from comments) as comment_table on posts.Id = comment_table.PostId
-where postsID = comment_table.PostId;
-END
